5ce315ee123307dae64c2db4b53a04701181c41e
Snory <robtill.in@gmail.com>
2023-02-03 05:21:45 +0100

    added StockItemData equality

--- Assets/Scripts/Stock/StockItem.cs
+++ Assets/Scripts/Stock/StockItem.cs
@@ -1,11 +1,7 @@
-﻿
-
-using System;
-using UnityEngine;
-
+﻿using System;
 
 [Serializable]
-public class StockItem
+public class StockItem : IEquatable<StockItem>
 {
     public ItemData ItemData;
     public float Amount;
@@ -13,4 +9,56 @@ public class StockItem
     public float TotalPrice;
     public ItemQuality ItemQuality;
     public ItemRarity ItemRarity;
+
+    public bool Equals(StockItem other)
+    {
+        if(other == null)
+        {
+            return false;
+        }
+
+        return
+                other.ItemData.Identification == this.ItemData.Identification
+                && other.ItemQuality == this.ItemQuality
+                && other.ItemRarity == this.ItemRarity;
+    }
+
+    public override bool Equals(Object obj)
+    {
+        if (obj == null)
+            return false;
+
+        StockItem stockItemObj = obj as StockItem;
+        if (stockItemObj == null)
+            return false;
+        else
+            return Equals(stockItemObj);
+    }
+
+    public static bool operator ==(StockItem stockItem1, StockItem stockItem2)
+    {
+        if (((object)stockItem1) == null || ((object)stockItem2) == null)
+            return Equals(stockItem1, stockItem2);
+
+        return stockItem1.Equals(stockItem2);
+    }
+
+    public static bool operator !=(StockItem stockItem1, StockItem stockItem2)
+    {
+        if (((object)stockItem1) == null || ((object)stockItem2) == null)
+            return ! Equals(stockItem1, stockItem2);
+
+        return ! stockItem1.Equals(stockItem2);
+    }
+
+
+    public override int GetHashCode()
+    {
+        return ItemData.Identification.GetHashCode() ^ ItemQuality.GetHashCode() ^ ItemRarity.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"Identification: {ItemData.Identification}, Quality: {ItemQuality}, Rarity: {ItemRarity}";
+    }
 }
---
+++ Assets/Scripts/Trade/Trade.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trade : MonoBehaviour
+{
+    private List<StockItem> _itemsToBuy;
+
+
+    public void OnTradeStockItemChanged(StockItem stockItem)
+    {
+
+    }
+}
